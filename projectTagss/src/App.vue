b44b52d74f2ea60fe39281d34936a10877128b40
Uziel Peñuela <uziel.penuela@prestamofeliz.com.mx>
2023-03-30 11:19:49 -0600

    ProjectTag - se suben cambios del proyecto

---
+++ projectTagss/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import InputTag from "./components/InputTag.vue";
+export default{
+  components:{
+    InputTag,
+  },
+  methods:{
+    handleOnTagsChange(tags){
+      console.log(tags);
+    },
+  },
+};
+</script>
+
+<template>
+  <InputTag :OnTagsChange="handleOnTagsChange"/>
+</template>
+
+<style scoped></style>
